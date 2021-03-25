720adec1e6082266546926e0b35cc8887ff3bbf7
Jake Aringay <jakeraymund.aringay@gmail.com>
2021-03-26 01:41:16 +1100

    Add scraper script for sword weapons

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const express = require("express");
 const morgan = require("morgan");
 const { port } = require("./config");
 
+// Call scraper script?
+const scrapeSwords = require("./scraper");
+
 // Set up application
 const app = express();
 
 // Monitor HTTP requests
 app.use(morgan("dev"));
+
+
+app.get("/", async (req, res) => {
+  const result = await scrapeSwords();
+  res.json(result);
+});
 
 // Run application
 app.listen(port, () => {
---
+++ scraper.js
@@ -0,0 +1,70 @@
+// Call required packages
+const cheerio = require("cheerio");
+const axios = require("axios").default;
+
+const fetchHtml = async url => {
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch (error) {
+    console.error(`ERROR: An error occurred while trying to fetch the URL: ${url}`);
+  }
+};
+
+const extractSwords = selector => {
+  const linkToSword = selector
+    .find("a[href*='/db/weapon/']")
+    .attr("href")
+    .trim();
+
+  const swordName = selector
+    .find("a[href*='/db/weapon/']")
+    .text()
+    .trim();
+
+  return { linkToSword, swordName };
+
+}
+
+const scrapeSwords = async () => {
+  const swordUrl = "https://genshin.honeyhunterworld.com/sword/"
+  const html = await fetchHtml(swordUrl);
+  const selector = cheerio.load(html);
+  // .wrappercont >
+  const searchResults = selector("body")
+    .find(".wrappercont > .art_stat_table");
+  
+  const swords = searchResults.map((index, element) => {
+    const elementSelector = selector(element);
+    return extractSwords(elementSelector);
+  })
+  .get();
+      
+  return swords;  
+}
+
+/* Backup code */
+// Weapon / Type / Rarity / ATK / Secondary / Passive / Bonus
+// const scrapeWeapons = async () => {
+//   const weaponUrl = "https://genshin.gg/weapons";
+
+//   const html = await fetchHtml(weaponUrl);
+
+//   const selector = cheerio.load(html);
+
+//   const searchResults = selector("body")
+//     .find(".rt-thead -header > .rt-tbody > .rt-tr-group");
+
+//   const weapons = searchResults.map((index, element) => {
+//     const elementSelector = selector(el);
+//     return extractWeapons(elementSelector);
+//   })
+//   .get();
+
+//   return weapons;
+
+// };
+
+
+
+module.exports = scrapeSwords;
